682fd1eb8e970bbbb58e467f72f2bb0fb68eaa9c
jameshamann <jameshamann@me.com>
2016-12-06 13:07:34 +0000

    adds test for buzz function

--- spec/fizzbuzz/core_spec.clj
+++ spec/fizzbuzz/core_spec.clj
@@ -6,3 +6,8 @@
   (it "should return fizz when passed a multiple of 3"
     (let [result(fizz 3)]
     (should= "Fizz!" result))))
+
+(describe "buzz"
+(it "should return buzz when passed a multiple of 5"
+  (let [result(buzz 10)]
+  (should= "Buzz!" result))))
